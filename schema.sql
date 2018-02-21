0ae3c02536c6a946eaea7d9d32c48bc4ea289ac7
Brandan Richardson <brandanr@gmail.com>
2018-02-21 15:00:34 -0800

    added schema.sql file

---
+++ schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE database bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products
+(
+    item_id INT(100) AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(100) NULL,
+    price DECIMAL(10,2) NULL,
+    stock_quantity INT(100) NULL,
+    PRIMARY KEY (item_id)
+);
+
+SELECT *
+FROM products;
