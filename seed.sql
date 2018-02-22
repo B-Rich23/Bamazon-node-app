16751010ecf2908bec0e4279be0c7b8703d812e5
Brandan Richardson <brandanr@gmail.com>
2018-02-21 21:13:14 -0800

    added seed.sql and bamazonCustomer.js file

---
+++ seed.sql
@@ -0,0 +1,52 @@
+USE bamazonDB;
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("peacoat", "Clothing", 125.50, 25);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("MacBook Pro", "Electronics", 2000.50, 10);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("loafers", "Clothing", 150.25, 20);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("vase", "Home", 30.25, 15);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("cutlery set", "Kitchen", 99.50, 55);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("cast iron skillet", "Kitchen", 45.00, 35);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("bananas", "Produce", 1.25, 54);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("clock", "Home", 50.00, 20);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("carrots", "Produce", 2.50, 45);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES
+    ("showerhead", "Bath", 75.25, 15);
+
