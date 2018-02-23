ad05bd202d64941c03ff7d07afee665db97b0892
Brandan Richardson <brandanr@gmail.com>
2018-02-23 08:16:42 -0800

    successfully added select id for stock_item to be updated in switch case #3

--- bamazonManager.js
+++ bamazonManager.js
@@ -44,9 +44,9 @@ function startApp() {
                 //     multiSearch();
                 //     break;
 
-                // case "Add to Inventory":
-                //     runSearch();
-                //     break;
+                case "Add to Inventory":
+                    runSearch();
+                    break;
 
                 // case "Search for a specific song":
                 //     songSearch();
@@ -73,85 +73,85 @@ function startApp() {
 //         });
 // }
 
-function multiSearch() {
-    var query = "SELECT stock_quantity FROM top5000 GROUP BY artist HAVING count(*) > 1";
-    connection.query(query, function (err, res) {
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].artist);
-        }
-        runSearch();
-    });
-}
+// function multiSearch() {
+//     var query = "SELECT stock_quantity FROM top5000 GROUP BY artist HAVING count(*) > 1";
+//     connection.query(query, function (err, res) {
+//         for (var i = 0; i < res.length; i++) {
+//             console.log(res[i].artist);
+//         }
+//         runSearch();
+//     });
+// }
 
-function rangeSearch() {
-    inquirer
-        .prompt([{
-            name: "start",
-            type: "input",
-            message: "Enter starting position: ",
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
-        },
-        {
-            name: "end",
-            type: "input",
-            message: "Enter ending position: ",
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
-        }
-        ])
-        .then(function (answer) {
-            var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-            connection.query(query, [answer.start, answer.end], function (err, res) {
-                for (var i = 0; i < res.length; i++) {
-                    console.log(
-                        "Position: " +
-                        res[i].position +
-                        " || Song: " +
-                        res[i].song +
-                        " || Artist: " +
-                        res[i].artist +
-                        " || Year: " +
-                        res[i].year
-                    );
-                }
-                runSearch();
-            });
-        });
-}
+// function rangeSearch() {
+//     inquirer
+//         .prompt([{
+//             name: "start",
+//             type: "input",
+//             message: "Enter starting position: ",
+//             validate: function (value) {
+//                 if (isNaN(value) === false) {
+//                     return true;
+//                 }
+//                 return false;
+//             }
+//         },
+//         {
+//             name: "end",
+//             type: "input",
+//             message: "Enter ending position: ",
+//             validate: function (value) {
+//                 if (isNaN(value) === false) {
+//                     return true;
+//                 }
+//                 return false;
+//             }
+//         }
+//         ])
+//         .then(function (answer) {
+//             var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+//             connection.query(query, [answer.start, answer.end], function (err, res) {
+//                 for (var i = 0; i < res.length; i++) {
+//                     console.log(
+//                         "Position: " +
+//                         res[i].position +
+//                         " || Song: " +
+//                         res[i].song +
+//                         " || Artist: " +
+//                         res[i].artist +
+//                         " || Year: " +
+//                         res[i].year
+//                     );
+//                 }
+//                 runSearch();
+//             });
+//         });
+// }
 
-function songSearch() {
-    inquirer
-        .prompt({
-            name: "song",
-            type: "input",
-            message: "What song would you like to look for?"
-        })
-        .then(function (answer) {
-            console.log(answer.song);
-            connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
-                console.log(
-                    "Position: " +
-                    res[0].position +
-                    " || Song: " +
-                    res[0].song +
-                    " || Artist: " +
-                    res[0].artist +
-                    " || Year: " +
-                    res[0].year
-                );
-                runSearch();
-            });
-        });
-}
+// function songSearch() {
+//     inquirer
+//         .prompt({
+//             name: "song",
+//             type: "input",
+//             message: "What song would you like to look for?"
+//         })
+//         .then(function (answer) {
+//             console.log(answer.song);
+//             connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
+//                 console.log(
+//                     "Position: " +
+//                     res[0].position +
+//                     " || Song: " +
+//                     res[0].song +
+//                     " || Artist: " +
+//                     res[0].artist +
+//                     " || Year: " +
+//                     res[0].year
+//                 );
+//                 runSearch();
+//             });
+//         });
+// }
 
 function readProducts() {
     console.log("\nInventory\n");
@@ -197,9 +197,9 @@ function howMuch() {
                 console.log("\n ");
                 order = answer.quantity;
                     updateStockItems();
-//             })
+            })
         });
-});
+};
 
 // function updateUnits() {
 //     var query = "UPDATE products SET units_ordered= " + order + " WHERE ?";
@@ -221,7 +221,7 @@ function updateStockItems() {
     connection.query(query, id, function (err, res) {
         readProducts();
     });
-}
+};
 // function readUpdatedAgain() {
 //     connection.query("SELECT * FROM products", function (err, res) {
 //         if (err) throw err;
